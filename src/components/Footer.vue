57958ad8551efa378fd6f09695a15c1525b88f77
apocelipes <seve3r@outlook.com>
2020-06-13 22:09:24 +0900

    added github buttons to Footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,23 @@
 <template>
-    <el-card class="footer">
+    <el-card class="footer" shadow="never">
         <div class="footer-container">
             <span>作者：{{authorMessage}}</span>
             <br>
             <span>特别鸣谢：{{contributorMessage}}</span>
             <br>
             <span>{{license}}</span>
+            <br>
+            <el-row style="margin: 10px 0">
+                <el-col :span="8">
+                <span>关注我们：</span>
+                    <el-link icon="iconfont el-icon-abgithub"
+                             href="https://github.com/ahoyButton/ahoy_button/">
+                    </el-link>
+                    <gh-btns-fork class="git-btn" slug="ahoyButton/ahoy_button" show-count></gh-btns-fork>
+                    <gh-btns-star class="git-btn" slug="ahoyButton/ahoy_button" show-count></gh-btns-star>
+                    <gh-btns-follow user="ahoyButton" show-count></gh-btns-follow>
+                </el-col>
+            </el-row>
         </div>
     </el-card>
 </template>
@@ -38,14 +50,20 @@
 
 <style scoped>
 .footer-container {
-    margin: 30px 70px;
+    margin: 30px 50px;
     text-align: left;
     font-style: italic;
+    color: aliceblue;
 }
 
 .footer {
+    background-color: #272727;
+    border: 0;
+    border-radius: 0;
     width: 100%;
     margin-top: 30px;
-    background-color: #272727;
+    margin-left: 0;
+    position: relative;
+    bottom: 0;
 }
 </style>
---
+++ src/plugins/vuegithubbutton.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import VueGitHubButtons from 'vue-github-buttons'
+import 'vue-github-buttons/dist/vue-github-buttons.css'
+
+Vue.use(VueGitHubButtons)
