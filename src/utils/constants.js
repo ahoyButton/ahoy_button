bafe28dd82ac41e236f24e4718bd243e3f7afc46
apocelipes <seve3r@outlook.com>
2020-06-30 16:09:45 +0900

    moved langs to constants.js

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,16 @@
 export const AUDIO_PREFIX = '/voices/'
+
+export const languages = [
+    {
+        title: '中文',
+        value: 'zh_CN'
+    },
+    {
+        title: 'English',
+        value: 'en'
+    },
+    {
+        title: '日本語',
+        value: 'ja_JP'
+    }
+]
