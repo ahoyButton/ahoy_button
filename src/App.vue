c0dd393019cad6efbae476c4b6ace9df325fe70b
apocelipes <seve3r@outlook.com>
2020-08-03 12:01:24 +0900

    set language automently

--- src/App.vue
+++ src/App.vue
@@ -11,15 +11,30 @@
 
 <script>
 export default {
-  name: 'app'
+    name: 'app',
+    created() {
+        this.$i18n.locale = this.getUserLang()
+    },
+    methods: {
+        getUserLang() {
+            const lang = navigator.language.toLowerCase().substr(0, 2)
+            switch (lang) {
+            case 'zh':
+                return 'zh_CN'
+            case 'ja':
+                return 'ja_JP'
+            default:
+                return 'en'
+            }
+        }
+    }
 }
 </script>
 
 <style lang="scss">
+    @import "styles/mixins";
+
     html,body,#app {
-        width: 100%;
-        height: 100%;
-        margin: 0;
-        padding: 0;
+        @include fullSize;
     }
 </style>
