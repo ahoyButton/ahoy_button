c414ccfeda47cff6488868731e646b2ea3f92ba2
apocelipes <seve3r@outlook.com>
2020-08-04 18:55:39 +0900

    added new-voice mark to config

--- src/genVoices.py
+++ src/genVoices.py
@@ -1,9 +1,37 @@
 import json
 import yaml
 
-
-with open('assets/voices.yaml') as configFile:
+confObj = None
+originObj = None
+with open('assets/voices.yaml') as configFile, open('assets/voices.json') as originFile:
     data = configFile.read()
     confObj = yaml.load(data, Loader=yaml.FullLoader)
-    with open('assets/voices.json', 'w') as target:
-        json.dump(confObj, target, indent=2)
+    oldData = originFile.read()
+    originObj = json.loads(oldData)
+
+
+def findGroup(iterable, target):
+    for group in iterable:
+        if group['group_name']['lang']['zh_CN'] == target['group_name']['lang']['zh_CN']:
+            return group
+    return None
+
+
+def addNewItemTag(oldData, newData):
+    for group in newData:
+        oldGroup = findGroup(oldData, group)
+        if not oldGroup:
+            group['isNew'] = True
+            for button in group['buttons']:
+                button['isNew'] = True
+        else:
+            for button in group['buttons']:
+                if button in oldGroup['buttons']:
+                    continue
+                else:
+                    button['isNew'] = True
+addNewItemTag(originObj, confObj)
+
+
+with open('assets/voices.json', 'w') as target:
+    json.dump(confObj, target, indent=2)
